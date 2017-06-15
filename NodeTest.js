5eda80ccc1efb08c5588c2cbdc70674990b78a95
Lisa <Lisa@lisa-s-mbp.fritz.box>
2017-06-15 11:53:17 +0200

    new Repository for Node

---
+++ NodeTest.js
@@ -0,0 +1,28 @@
+"use strict";
+console.log("Server starting");
+const Http = require("http");
+const Url = require("url");
+let port = process.env.PORT;
+if (port == undefined)
+    port = 8100;
+let server = Http.createServer();
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
+function handleListen() {
+    console.log("Listening on port: " + port);
+}
+function handleRequest(_request, _response) {
+    console.log("Request received");
+    console.log(_request.url);
+    let query = Url.parse(_request.url, true).query;
+    console.log(query);
+    let key;
+    for (key in query)
+        console.log(key + ":" + query[key]);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write("Ich höre Stimmen!");
+    _response.end();
+}
+//# sourceMappingURL=NodeTest.js.map
