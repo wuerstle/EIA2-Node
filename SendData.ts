711f285ae534146a8d2af92e185732804d1166fe
Lisa <Lisa@192.168.0.13>
2017-06-21 09:13:10 +0200

    heroku app verlinkt

--- SendData.ts
+++ SendData.ts
@@ -23,7 +23,7 @@ namespace SendData {
 
     function sendRequest(_color: string): void {
         let xhr: XMLHttpRequest = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8100?color=" + _color, true);
+        xhr.open("GET", "https://eia2-test.herokuapp.com/?color" + _color, true);
         //xhr.open("GET", "https://eia2-servertest.herokuapp.com?color=" + _color, true);
         xhr.addEventListener("readystatechange", handleStateChange);
         xhr.send();
