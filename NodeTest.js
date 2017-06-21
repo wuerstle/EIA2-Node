e7c9fc5b9ae988d816aafd57c775821ec6b63ca7
Lisa <Lisa@192.168.0.13>
2017-06-21 08:41:35 +0200

    Ausgabe auf Heroku hinzugefügt

--- NodeTest.js
+++ NodeTest.js
@@ -22,7 +22,21 @@ function handleRequest(_request, _response) {
         console.log(key + ":" + query[key]);
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    _response.write("Ich höre Stimmen!");
+    _response.write("Your IceCream: <br>");
+    _response.write("Chocolate:" + query["Chocolate"] + "<br>");
+    _response.write("Vanilla:" + query["Vanilla"] + "<br>");
+    _response.write("Strawberry:" + query["Strawberry"] + "<br>");
+    _response.write("Raspberry:" + query["Raspberry"] + "<br>");
+    _response.write("Bubblegum:" + query["Bubblegum"] + "<br>");
+    _response.write("Dark Chocolate:" + query["Dark Chocolate"] + "<br>");
+    _response.write("White Chocolate:" + query["White Chocolate"] + "<br>");
+    _response.write("Yogurt:" + query["Yogurt"] + "<br>");
+    _response.write("Kiddy Chocolate:" + query["Kiddy Chocolate"] + "<br>");
+    _response.write("Banana:" + query["Banana"] + "<br>");
+    _response.write("Blueberry:" + query["Blueberry"] + "<br>");
+    _response.write("Your Toppings:" + query["Toppings"]);
+    _response.write("Your Box:" + query["RadioBoxes"]);
+    _response.write("Delivery Option:" + query["RadioBoxes2"]);
     _response.end();
 }
 //# sourceMappingURL=NodeTest.js.map
