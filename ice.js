fc650382b0d1ce2e0532544fbebba5ba3caab0a4
Lisa <Lisa@lisa-s-mbp.fritz.box>
2017-06-25 15:25:56 +0200

    added string

--- ice.js
+++ ice.js
@@ -5,6 +5,7 @@
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 "use strict";
 console.log("Server starting");
+var flavours = ["Chocolate", "Vanilla", "Strawberry", "Raspberry", "Bubblegum", "Dark Chocolate", "White Chocolate", "Yogurt", "Kiddy Chocolate", "Banana", "Blueberry"];
 var Http = require("http");
 var Url = require("url");
 var port = process.env.PORT;
@@ -22,25 +23,25 @@ function handleRequest(_request, _response) {
     console.log(_request.url);
     var query = Url.parse(_request.url, true).query;
     console.log(query);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
     var key;
     for (key in query) {
         console.log(key + ":" + query[key]);
     }
-    _response.setHeader("Access-Control-Allow-Origin", "*");
-    _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.write("YOUR ICECREAM <hr>");
-    //        _response.write("Chocolate:" + query["Chocolate"] + "<br>");
-    //        _response.write("Vanilla:" + query["Vanilla"] + "<br>");
-    //        _response.write("Strawberry:" + query["Strawberry"] + "<br>");
-    //        _response.write("Raspberry:" + query["Raspberry"] + "<br>");
-    //        _response.write("Bubblegum:" + query["Bubblegum"] + "<br>");
-    //        _response.write("Dark Chocolate:" + query["Dark Chocolate"] + "<br>");
-    //        _response.write("White Chocolate:" + query["White Chocolate"] + "<br>");
-    //        _response.write("Yogurt:" + query["Yogurt"] + "<br>");
-    //        _response.write("Kiddy Chocolate:" + query["Kiddy Chocolate"] + "<br>");
-    //        _response.write("Banana:" + query["Banana"] + "<br>");
-    //        _response.write("Blueberry:" + query["Blueberry"] + "<br>");
-    _response.write("Your Flavours:" + query["NumberFlavours"] + "<br>");
+    _response.write("Chocolate:" + query["Chocolate"] + "<br>");
+    _response.write("Vanilla:" + query["Vanilla"] + "<br>");
+    _response.write("Strawberry:" + query["Strawberry"] + "<br>");
+    _response.write("Raspberry:" + query["Raspberry"] + "<br>");
+    _response.write("Bubblegum:" + query["Bubblegum"] + "<br>");
+    _response.write("Dark Chocolate:" + query["Dark Chocolate"] + "<br>");
+    _response.write("White Chocolate:" + query["White Chocolate"] + "<br>");
+    _response.write("Yogurt:" + query["Yogurt"] + "<br>");
+    _response.write("Kiddy Chocolate:" + query["Kiddy Chocolate"] + "<br>");
+    _response.write("Banana:" + query["Banana"] + "<br>");
+    _response.write("Blueberry:" + query["Blueberry"] + "<br>");
+    _response.write("<br>");
     _response.write("Your Toppings:" + query["Toppings"] + "<br>");
     _response.write("Your Box:" + query["RadioBoxes"] + "<br>");
     _response.write("Delivery Option:" + query["RadioBoxes2"]);
--- ice.ts
+++ ice.ts
@@ -6,6 +6,7 @@
 
 
 console.log("Server starting");
+let flavours: string[] = ["Chocolate", "Vanilla", "Strawberry", "Raspberry", "Bubblegum", "Dark Chocolate", "White Chocolate", "Yogurt", "Kiddy Chocolate", "Banana", "Blueberry"];
 
 import Http = require("http");
 import Url = require("url");
@@ -33,28 +34,33 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
     console.log(_request.url);
     let query: AssocStringString = Url.parse(_request.url, true).query;
     console.log(query);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
     let key: string;
     for (key in query) {
         console.log(key + ":" + query[key]);
     }
 
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.write("YOUR ICECREAM <hr>");
-//        _response.write("Chocolate:" + query["Chocolate"] + "<br>");
-//        _response.write("Vanilla:" + query["Vanilla"] + "<br>");
-//        _response.write("Strawberry:" + query["Strawberry"] + "<br>");
-//        _response.write("Raspberry:" + query["Raspberry"] + "<br>");
-//        _response.write("Bubblegum:" + query["Bubblegum"] + "<br>");
-//        _response.write("Dark Chocolate:" + query["Dark Chocolate"] + "<br>");
-//        _response.write("White Chocolate:" + query["White Chocolate"] + "<br>");
-//        _response.write("Yogurt:" + query["Yogurt"] + "<br>");
-//        _response.write("Kiddy Chocolate:" + query["Kiddy Chocolate"] + "<br>");
-//        _response.write("Banana:" + query["Banana"] + "<br>");
-//        _response.write("Blueberry:" + query["Blueberry"] + "<br>");
-        _response.write("Your Flavours:" + query["NumberFlavours"] + "<br>");
-        _response.write("Your Toppings:" + query["Toppings"] + "<br>");
-        _response.write("Your Box:" + query["RadioBoxes"] + "<br>");
-        _response.write("Delivery Option:" + query["RadioBoxes2"]);
-        _response.end();
-    }
+
+    _response.write("YOUR ICECREAM <hr>");
+    _response.write("Chocolate:" + query["Chocolate"] + "<br>");
+    _response.write("Vanilla:" + query["Vanilla"] + "<br>");
+    _response.write("Strawberry:" + query["Strawberry"] + "<br>");
+    _response.write("Raspberry:" + query["Raspberry"] + "<br>");
+    _response.write("Bubblegum:" + query["Bubblegum"] + "<br>");
+    _response.write("Dark Chocolate:" + query["Dark Chocolate"] + "<br>");
+    _response.write("White Chocolate:" + query["White Chocolate"] + "<br>");
+    _response.write("Yogurt:" + query["Yogurt"] + "<br>");
+    _response.write("Kiddy Chocolate:" + query["Kiddy Chocolate"] + "<br>");
+    _response.write("Banana:" + query["Banana"] + "<br>");
+    _response.write("Blueberry:" + query["Blueberry"] + "<br>");
+    
+    _response.write("<br>");
+    
+    _response.write("Your Toppings:" + query["Toppings"] + "<br>");
+    
+
+    _response.write("Your Box:" + query["RadioBoxes"] + "<br>");
+    _response.write("Delivery Option:" + query["RadioBoxes2"]);
+    _response.end();
+}
